2362702c8613b58bc1bd878ca01e02b2e90b9c6a
harshaak2 <sriharshakurra15@gmail.com>
2023-12-28 02:30:29 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+
 dotenv.config();
 
 mongoose
@@ -17,3 +19,11 @@ const app = express();
 app.listen(3000, () => {
     console.log("Server is running on port 3000.");
 });
+
+// request is the data that we get from the client side
+// response is the data that we send back from the server side
+//? this sets up an with a single GET endpoint '/test' that sends a Hello World response and 
+//? then integrates this router with an Express application to handle requests starting with /api/user
+//* Express router is a way to organize and modularize your route handling in an Express.js application.
+//* It provides a more structured approach for handling different routes and HTTP methods by splitting them into separate modules or files
+app.use('/api/user', userRouter);
