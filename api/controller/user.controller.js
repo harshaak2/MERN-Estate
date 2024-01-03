d01fc35e37f561e9156b1790bc4c0b423c526194
harshaak2 <sriharshakurra15@gmail.com>
2024-01-03 22:28:37 +0530

    create user update api route

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,3 +1,37 @@
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import User from '../models/user.model.js';
+
 export const test = (req, res) => {
     res.send("Hello World from user controller");
+}
+
+export const updateUser = async (req, res) => {
+    //* the first parameter is the one we are getting from jwt.verifyToken in verifyUser.js 
+    //* the second parameter is the one we are getting from the route 
+    if(req.user.id !== req.params.id){
+        return nextTick(errorHandler(401, 'You can only update your own account!'))
+    }
+    
+    try {
+        if(req.body.password){
+            req.body.password = bcrypt.hashSync(req.body.password, 10);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+            }
+        }, {new: true})
+
+        const {hashedPassword: pass, ...rest} = updatedUser._doc;
+
+        return res.status(200).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
 }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { test } from "../controller/user.controller.js";
+import { test, updateUser } from "../controller/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+// /update/:id is set up to listen for POST requests
+// verifyToken is a middleware function and updateUser is a handler function
+router.post('/update/:id', verifyToken, updateUser)
 
 export default router;
